446e19361e22b082131e86b4e165e68842606d60
ryotairi <ryotairi.383e@extera.xyz>
2026-02-02 09:04:14 +0500

    feat: preview topic in chat invitations

--- lib/pages/chat_list/invite_dialog.dart
+++ lib/pages/chat_list/invite_dialog.dart
@@ -2,11 +2,14 @@ import 'package:extera_next/config/app_config.dart';
 import 'package:extera_next/config/setting_keys.dart';
 import 'package:extera_next/generated/l10n/l10n.dart';
 import 'package:extera_next/utils/localized_exception_extension.dart';
+import 'package:extera_next/utils/url_launcher.dart';
 import 'package:extera_next/widgets/avatar.dart';
 import 'package:extera_next/widgets/future_loading_dialog.dart';
 import 'package:extera_next/widgets/list_divider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
 import 'package:go_router/go_router.dart';
+import 'package:linkify/linkify.dart';
 import 'package:matrix/matrix.dart';
 
 class InviteDialog extends StatelessWidget {
@@ -36,7 +39,6 @@ class InviteDialog extends StatelessWidget {
         .getState(EventTypes.RoomMember, room.client.userID!)
         ?.senderId;
     context.go('/rooms/settings/security/ignorelist', extra: userId);
-    Navigator.of(context, rootNavigator: true).pop();
   }
 
   @override
@@ -47,6 +49,11 @@ class InviteDialog extends StatelessWidget {
       EventTypes.RoomMember,
       room.client.userID!,
     );
+    final topic = room.topic;
+    final reason = membershipEvent?.content.tryGet<String>('reason');
+    final actualMembersCount =
+        (room.summary.mInvitedMemberCount ?? 0) +
+        (room.summary.mJoinedMemberCount ?? 0);
 
     return Scaffold(
       appBar: AppBar(title: Text(L10n.of(context).newInvitation)),
@@ -78,12 +85,76 @@ class InviteDialog extends StatelessWidget {
                               context,
                             ).youInvitedBy(membershipEvent.senderId),
                           )
-                        : null,
+                        : room.directChatMatrixID != null
+                        ? Text(L10n.of(context).newChatRequest)
+                        : Text(L10n.of(context).inviteGroupChat),
                   ),
                 ],
               ),
             ),
           ),
+          if (reason != null && reason.isNotEmpty)
+            Padding(
+              padding: const .all(8),
+              child: Material(
+                clipBehavior: .hardEdge,
+                color: theme.colorScheme.surfaceContainerHigh,
+                borderRadius: borderRadius,
+                child: ListTile(
+                  title: Text(reason),
+                  subtitle: Text(L10n.of(context).reason),
+                  leading: const Icon(Icons.wysiwyg),
+                ),
+              ),
+            ),
+          if (topic.isNotEmpty)
+            Padding(
+              padding: const .all(8),
+              child: Material(
+                clipBehavior: .hardEdge,
+                color: theme.colorScheme.surfaceContainerHigh,
+                borderRadius: borderRadius,
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text(
+                        L10n.of(context).chatDescription,
+                        style: TextStyle(
+                          color: theme.colorScheme.secondary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      title: SelectableLinkify(
+                        text: room.topic.isEmpty
+                            ? L10n.of(context).noChatDescriptionYet
+                            : room.topic,
+                        textScaleFactor: MediaQuery.textScalerOf(
+                          context,
+                        ).scale(1),
+                        options: const LinkifyOptions(humanize: false),
+                        linkStyle: const TextStyle(
+                          color: Colors.blueAccent,
+                          decorationColor: Colors.blueAccent,
+                        ),
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontStyle: room.topic.isEmpty
+                              ? FontStyle.italic
+                              : FontStyle.normal,
+                          color: theme.textTheme.bodyMedium!.color,
+                          decorationColor: theme.textTheme.bodyMedium!.color,
+                        ),
+                        onOpen: (url) =>
+                            UrlLauncher(context, url.url).launchUrl(),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                  ],
+                ),
+              ),
+            ),
           Padding(
             padding: const .all(8),
             child: Material(
