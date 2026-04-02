da9a866b290e1b695ba3e9fc05b1973bc0c25b37
ryotairi <ryotairi.383e@extera.xyz>
2026-04-02 22:05:46 +0500

    use jit.si instance

--- lib/config/setting_keys.dart
+++ lib/config/setting_keys.dart
@@ -14,7 +14,7 @@ abstract class SettingKeys {
 
 enum AppSettings<T> {
   experimentalJitsi<bool>('xyz.extera.jitsi', false),
-  jitsiDomain<String>('xyz.extera.jitsi_domain', 'meet.element.io'),
+  jitsiDomain<String>('xyz.extera.jitsi_domain', 'meet.jit.si'),
   applicationName<String>('xyz.extera.app_name', 'Extera'),
   logoUrl<String>('xyz.extera.logo_url', 'https://extera.xyz/logo.svg'),
   privacyPolicy<String>('xyz.extera.tos', 'https://github.com/ExteraApp/Extera/blob/main/PRIVACY.md'),
