f14f49d49b6a53ca6ed4c49588dbb18f5f6d1b0e
ryotairi <ryotairi.383e@extera.xyz>
2026-04-07 23:34:17 +0500

    follow-up remove dedup

--- lib/pages/chat_search/chat_search_page.dart
+++ lib/pages/chat_search/chat_search_page.dart
@@ -100,16 +100,6 @@ class ChatSearchController extends State<ChatSearchPage>
               result.$2,
             ),
           )
-          // Deduplication workaround for
-          // https://github.com/famedly/matrix-dart-sdk/issues/1831
-          .map(
-            (result) => (
-              <String, Event>{
-                for (final event in result.$1) event.eventId: event,
-              }.values.toList(),
-              result.$2,
-            ),
-          )
           .asBroadcastStream();
     });
   }
@@ -137,16 +127,6 @@ class ChatSearchController extends State<ChatSearchPage>
                 if (previousSearchResult != null) ...previousSearchResult,
                 ...result.$1,
               ],
-              result.$2,
-            ),
-          )
-          // Deduplication workaround for
-          // https://github.com/famedly/matrix-dart-sdk/issues/1831
-          .map(
-            (result) => (
-              <String, Event>{
-                for (final event in result.$1) event.eventId: event,
-              }.values.toList(),
               result.$2,
             ),
           )
