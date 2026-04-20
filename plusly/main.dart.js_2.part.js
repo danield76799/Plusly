7c647ab709077fe21b5d67a82494ab36acc7b1f4
danield76799 <danield76799@users.noreply.github.com>
2026-04-20 17:58:11 +0000

    deploy: c8a5ffcba34cef8511ea94c6342d0292625892e3

---
+++ plusly/main.dart.js_2.part.js
@@ -0,0 +1,10 @@
+((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
+$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={bk:function bk(){}}
+B=c[0]
+A=a.updateHolder(c[52],A)
+A.bk.prototype={
+E(d,e){return this.gbD(this).$1(e)}}
+var z=a.updateTypes([]);(function inheritance(){var y=a.inherit
+y(A.bk,B.H)})()};
+(a=>{a["xvULtGmhHHdcZKJjZeGDIy24Xos="]=a.current})($__dart_deferred_initializers__);
+//# sourceMappingURL=main.dart.js_2.part.js.map
