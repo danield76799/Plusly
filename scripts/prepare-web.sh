ef0479dd5b0d1fcb3461c6821563a286cf9745fd
ryotairi <ryotairi.383e@extera.xyz>
2026-05-03 03:11:01 +0500

    update prepare web script

--- scripts/prepare-web.sh
+++ scripts/prepare-web.sh
@@ -1,10 +1,20 @@
 #!/bin/sh -ve
-rm -rf assets/js/package
 
-OLM_VERSION=$(cat pubspec.yaml | yq .dependencies.flutter_olm)
-DOWNLOAD_PATH="https://github.com/famedly/olm/releases/download/v$OLM_VERSION/olm.zip"
+version=$(yq ".dependencies.flutter_vodozemac" < pubspec.yaml)
+version=$(expr "$version" : '\^*\(.*\)')
+git clone https://github.com/famedly/dart-vodozemac.git -b ${version} .vodozemac
+cd .vodozemac
+cargo install flutter_rust_bridge_codegen
+flutter_rust_bridge_codegen build-web --dart-root dart --rust-root $(readlink -f rust) --release
+cd ..
+rm -f ./assets/vodozemac/vodozemac_bindings_dart*
+mv .vodozemac/dart/web/pkg/vodozemac_bindings_dart* ./assets/vodozemac/
+rm -rf .vodozemac
 
-cd assets/js/ && curl -L $DOWNLOAD_PATH > olm.zip && cd ../../
-cd assets/js/ && unzip olm.zip && cd ../../
-cd assets/js/ && rm olm.zip && cd ../../
-cd assets/js/ && mv javascript package && cd ../../
+# Add native imaging:
+cd web/
+curl -L 'https://github.com/famedly/dart_native_imaging/releases/download/v0.2.1/native_imaging.zip' > native_imaging.zip # make sure to sync version with pubspec.yaml
+unzip native_imaging.zip
+mv js/* .
+rmdir js
+rm native_imaging.zip
