3fd356087b2ac54fd03bab032adfd203f734f82a
root <root@vm8748.lumadock.com>
2026-05-08 14:55:50 +0000

    Fix: Cache sender user in Message widget to avoid FutureBuilder rebuilds

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -9,6 +9,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   desktop_webview_window
   dynamic_color
   file_selector_windows
+  firebase_core
   flutter_secure_storage_windows
   flutter_webrtc
   geolocator_windows
