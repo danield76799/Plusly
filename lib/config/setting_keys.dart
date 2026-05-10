aa15228d2663b906e417a67a9fb00bd8e9dec48b
root <root@vm8748.lumadock.com>
2026-05-10 18:06:26 +0000

    Fix translation service - switch to LibreTranslate API

--- lib/config/setting_keys.dart
+++ lib/config/setting_keys.dart
@@ -34,7 +34,7 @@ enum AppSettings<T> {
   translationTargetLanguage<String>('xyz.plusly.translationTargetLanguage', ''),
   exteraServiceUrl<String>(
     'xyz.plusly.serviceUrl',
-    'https://plusly.im/service',
+    'https://libretranslate.de', // Free open source translation API
   ),
   latexMath<bool>('xyz.plusly.latexMath', false),
   messageTranslation<bool>('xyz.plusly.messageTranslation', true),
