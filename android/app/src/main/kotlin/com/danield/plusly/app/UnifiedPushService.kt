b8c070fdacd6a855f831ca7740c0d4548e9add92
danield76799 <daanentamara+1@gmail.com>
2026-05-12 19:41:48 +0200

    Fix UnifiedPushService.kt with proper newlines

--- android/app/src/main/kotlin/com/danield/plusly/app/UnifiedPushService.kt
+++ android/app/src/main/kotlin/com/danield/plusly/app/UnifiedPushService.kt
@@ -4,7 +4,6 @@ package com.danield.plusly.app
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.engine.dart.DartExecutor
 import org.unifiedpush.flutter.connector.UnifiedPushReceiver
-
 import android.content.Context
 
 class UnifiedPushReceiver : UnifiedPushReceiver() {
@@ -12,12 +11,8 @@ class UnifiedPushReceiver : UnifiedPushReceiver() {
         var engine = MainActivity.engine
         if (engine == null) {
             engine = MainActivity.provideEngine(context)
-            engine.localizationPlugin.sendLocalesToFlutter(
-                context.resources.configuration
-            )
-            engine.dartExecutor.executeDartEntrypoint(
-                DartExecutor.DartEntrypoint.createDefault()
-            )
+            engine.localizationPlugin.sendLocalesToFlutter(context.resources.configuration)
+            engine.dartExecutor.executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault())
         }
         return engine
     }
