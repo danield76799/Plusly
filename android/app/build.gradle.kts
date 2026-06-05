624490007fecdaae73f0cbc3d816691ca6bbb739
root <root@vm8748.lumadock.com>
2026-06-05 05:18:11 +0000

    fix: resolve libc++_shared.so conflict with flutter_pdfview

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -98,6 +98,12 @@ android {
             enableSplit = false
         }
     }
+
+    packaging {
+        jniLibs {
+            pickFirsts += ["lib/arm64-v8a/libc++_shared.so", "lib/armeabi-v7a/libc++_shared.so", "lib/x86_64/libc++_shared.so"]
+        }
+    }
 }
 
 flutter {
