5b25a1bc427b85ba0bb809817bab2a61b1b5835e
Daan <danield76799@gmail.com>
2026-07-02 10:00:15 +0200

    fix: add missing Material import for Icons
    Icons class requires flutter/material.dart import.

--- lib/features/push/data/unified_push_provider.dart
+++ lib/features/push/data/unified_push_provider.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 
 import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:matrix/matrix.dart';
 import 'package:shared_preferences/shared_preferences.dart';
